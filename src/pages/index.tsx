4a8321148ce7bee959f412a1282bface9c592d5f
Rurik Pinheiro <rurikspin@gmail.com>
2020-09-20 22:49:22 -0300

    feat: customize homepage

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { useStaticQuery, graphql } from "gatsby"
+import { FixedObject } from "gatsby-image"
+import React from "react"
+
+import Image from "../components/image"
+
+import "./styles.css"
+
+interface GraphqlQuery {
+  fileName: {
+    childImageSharp: {
+      fixed: FixedObject
+    }
+  }
+}
+
+export default function Home() {
+  const data: GraphqlQuery = useStaticQuery(graphql`
+    query {
+      fileName: file(relativePath: { eq: "profile.png" }) {
+        childImageSharp {
+          fixed(width: 252, height: 252) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <>
+      <div className="profile-img">
+        <Image fixed={data.fileName.childImageSharp.fixed} rounded />
+      </div>
+      <div className="profile-title">
+        <p>Hi, there 👋</p>
+      </div>
+      <div className="profile-description">
+        <p>
+          Welcome to my personal page. I'm <b>Rurik Pinheiro</b> and this is
+          where I put all my programming stuffs.
+        </p>
+      </div>
+    </>
+  )
+}
